724821f307225b4f9ee2988f0e00bb6990633b4a
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-09-09 17:41:41 +0530

    Fix email formatting in connection request notification

--- src/routes/request.js
+++ src/routes/request.js
@@ -44,8 +44,8 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async(req, res) 
         const data = await connectionRequest.save();
 
         const emailRes = await sendEmail.run(
-            "New Connect Request from" + req.user.firstName,
-            req.user.firstName + "is" + status + "in" + toUser.firstName
+            "New Connect Request from" + " " + req.user.firstName,
+            req.user.firstName + " " + "is" + " " + status + " " + "in" + " " + toUser.firstName
         );
         console.log("Email sent successfully!!", emailRes);
 
